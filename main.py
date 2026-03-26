ba549f7dc113397c4e71cd84afa4306d44ac6581
juanCabral-DJ <juandavidcabral71@gmail.com>
2026-03-26 11:35:10 -0700

    resolviendo problemas post conflictos

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import auth_routes, testimonial_routes
+from app.router.router_products import router as router_products
+from app.router.router_category import router as router_category
+from app.router.router_warranty import router as router_warranty
+from app.router.router_brand import router as router_brand
+from app.router.router_condition import router as router_condition
+from app.router.router_capacity import router as router_capacity
 
 app = FastAPI(
     title="Web'Catalog API",
@@ -28,17 +34,6 @@ def read_root():
 
 app.include_router(auth_routes.router)
 app.include_router(testimonial_routes.router)
-from fastapi import FastAPI
-
-from app.router.router_products import router as router_products
-from app.router.router_category import router as router_category
-from app.router.router_warranty import router as router_warranty
-from app.router.router_brand import router as router_brand
-from app.router.router_condition import router as router_condition
-from app.router.router_capacity import router as router_capacity
-
-app = FastAPI()
-
 app.include_router(router_products)
 app.include_router(router_category)
 app.include_router(router_warranty)
